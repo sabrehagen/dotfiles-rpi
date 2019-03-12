66f275e8866399bee9f409343327f4d92093d180
Daniel Matuschek <info@hifiberry.com>
2019-03-12 08:49:12 +0000

    setup

--- .config/rpi/setup.sh
+++ .config/rpi/setup.sh
@@ -4,10 +4,6 @@ if [ $USER != "root" ]; then
   exit 1
 fi
 
-# Install onedrive client
-wget -O /usr/local/bin/onedrive -nv https://github.com/sabrehagen/onedrive/raw/master/onedrive.rpi && \
-  chmod +x /usr/local/bin/onedrive
-
 # Install musikcube
 wget -O musickube.deb -nv https://github.com/clangen/musikcube/releases/download/0.63.0/musikcube_0.63.0_rpi_stretch_armhf.deb && \
   dpkg -i musickube.deb
@@ -22,3 +18,9 @@ vcsh clone https://sabrehagen@github.com/sabrehagen/dotfiles-tmux.git
 vcsh clone https://sabrehagen@github.com/sabrehagen/dotfiles-tmuxinator.git
 vcsh clone https://sabrehagen@github.com/sabrehagen/dotfiles-xdg.git
 vcsh clone https://sabrehagen@github.com/sabrehagen/dotfiles-zsh.git
+
+# Install rpi applications
+cp -r ~/.config/rpi/builds/omxplayer /
+
+# Check for updates
+sudo rpi-update
